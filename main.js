2fdd901b17c2947e5b02d63bfb764b459dfa3bde
a3a2a <81283225+a3a2a@users.noreply.github.com>
2021-04-11 23:58:05 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/log_in.html';
+    }
+    if(request.url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
 
-var app = http.createServer(function(request, response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  console.log(_url);
-  if(_url == '/'){
-    _url='/login.html';
-  }
-  if(_url == 'favicon.ico'){
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  response.end(fs.readFileSync(_dirname + _url));
 });
 app.listen(3000);
